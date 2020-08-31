cff7d1cff6f15a18a98278ebcf3be61beb66176a
Unknown <agusgc_99@hotmail.com>
2020-08-31 15:11:48 -0300

    Rutas y screens

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HomeScreen = () => {
+	return (
+		<div>
+			<h1>HOLA MUNDO Soy el home Screen</h1>
+		</div>
+	);
+};
---
+++ frontend/src/screens/PostScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const PostScreen = ()=> {
+    
+    return (
+        <div>
+        <h1>HOLA MUNDO Soy el PostScreen </h1>
+    </div>
+    );
+}
