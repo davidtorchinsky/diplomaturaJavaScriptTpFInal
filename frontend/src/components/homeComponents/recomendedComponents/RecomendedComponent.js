fd32b3f9da1045d276051722cc1fb26c2f580cb6
TIKINNI <tikinni97@gmail.com>
2020-09-09 22:54:56 -0300

    recomendados

--- frontend/src/components/homeComponents/recomendedComponents/RecomendedComponent.js
+++ frontend/src/components/homeComponents/recomendedComponents/RecomendedComponent.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
 
-export function Recomendados() {
-    return (
-        <div className="recommended">
-              <img  className="thumbnail" src={require("../../../assets/images/lotr.jpg")} alt="A meme"/>
-           
-            <div className="recommended-description">
-                <div className="header">
-                    <span className="title">Titulo del meme</span>
-                    <span className="likes">5</span>
+export function Recomendado({recomendado}) {
+    if (recomendado) {
+        console.log(recomendado);
+        return (
+            <div className="recommended">
+                <img className="thumbnail" src={require("../../../assets/images/lotr.jpg")} alt="A meme" />
+
+                <div className="recommended-description">
+                    <div className="header">
+                        <span className="title">{recomendado.titulo}</span>
+                        <span className="likes">{recomendado.likes}</span>
+                    </div>
+                    <p className="author">{`by @${recomendado.author}`}</p>
+                    <p className="category">{recomendado.categorias.map((elem) => {
+                        return ` ${elem.nombre}`;
+                    })}</p>
                 </div>
-                <p className="author">by @nakosuke</p>
-                <p className="category">Categoria1, Categoria2,</p>
             </div>
-        </div>
-    );
+        ); 
+    } else {
+        return (
+            <div><p>No hay recomendados disponibles.</p></div>
+        );
+    }
+    
 }
