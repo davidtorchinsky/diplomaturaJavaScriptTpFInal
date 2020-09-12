b8f4a092893fd05479e1088689facaa7f3839c1d
Unknown <agusgc_99@hotmail.com>
2020-09-12 17:44:23 -0300

    Revert "Un poco de estilo"
    This reverts commit 4ebeaa013bb523e9a539ae87a6aa03a56d6b567f.

--- frontend/src/components/homeComponents/recomendedComponents/RecomendedComponent.js
+++ frontend/src/components/homeComponents/recomendedComponents/RecomendedComponent.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 
-export const RecomendedComponent = ()=> {
+export function Recomendado({recomendado}) {
+    if (recomendado) {
+        console.log(recomendado);
+        return (
+            <div className="recommended">
+                <img className="thumbnail" src={require("../../../assets/images/lotr.jpg")} alt="A meme" />
+
+                <div className="recommended-description">
+                    <div className="header">
+                        <span className="title">{recomendado.titulo}</span>
+                        <span className="likes">{recomendado.likes}</span>
+                    </div>
+                    <p className="author">{`by @${recomendado.author}`}</p>
+                    <p className="category">{recomendado.categorias.map((elem) => {
+                        return ` ${elem.nombre}`;
+                    })}</p>
+                </div>
+            </div>
+        ); 
+    } else {
+        return (
+            <div><p>No hay recomendados disponibles.</p></div>
+        );
+    }
     
-    return (
-        <div>
-            <h1>RecomendedComponent working...</h1>
-        </div>
-    );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 
-import { PostComponent } from "../components/postComponents/PostComponent";
+import { PostComponent } from '../components/postComponents/PostComponent';
+import { CategoriaComponent } from '../components/homeComponents/CategoriaComponent';
+import { getAllPost } from '../services/PostService';
+import { RecomendadosList } from '../components/homeComponents/recomendedComponents/RecomendadosList';
+import { Hidden } from '@material-ui/core';
+
+const recomendados = [{ titulo: "Algo", likes: '10', author: 'nakosuke', categorias: [{ nombre: 'Categoria1' }, { nombre: 'Categoria2' }] },
+	{ titulo: "Otro", likes: '50', author: 'aru', categorias: [{ nombre: 'Categoria5' }] },
+	{ titulo: "Y uno mas con nombre largo a ver que pasa cuando no entra en el div", likes: '50', author: 'unusuariodelapagina', categorias: [{ nombre: 'Categoria5' }, { nombre: 'Categoria10' }, {nombre: 'Categoria8'}, {nombre: 'Categoria9'}, {nombre: 'Politica y weas'}, {nombre: 'Categoria5'}, ] },
+];
+
 
 export const HomeScreen = () => {
+	let posts = getAllPost();
+
+	const [ categoriaClick, setCategoriaClick ] = useState('All');
+	const [ postsAMostrar, setPostsAMostrar ] = useState(posts);
+	const [ widthChange, setWidthChange ] = useState(false);
+	const [ isMobile, setisMobile ] = useState(false);
+
+	useEffect(
+		() => {
+			let filterArray = posts.filter((post) => {
+				return categoriaClick == 'All' || categoriaClick == post.categoria;
+			});
+			setPostsAMostrar(filterArray);
+		},
+		[ categoriaClick ]
+	);
+
 	return (
-		<div className='homeContainer'>
-			<Grid container spacing={3}  >
-				<Grid item xs={6} sm={3}>
-					<p>Categorias</p>
+		<div className="homeContainer">
+			<Grid container spacing={0}>
+				<Hidden xsDown>
+					<Grid item xs={0} sm={2} md={2}>
+						<CategoriaComponent setCategoriaClick={setCategoriaClick} />
+					</Grid>
+				</Hidden>
+				<Grid item xs={12} sm={9}  md={6} className="postContainer">
+					<ul>
+						{postsAMostrar.map((post) => {
+							return (
+								<li key={post}>
+									<PostComponent post={post} />
+								</li>
+							);
+						})}
+					</ul>
 				</Grid>
-				<Grid item xs={12} sm={6} className='postContainer'>
-					<PostComponent></PostComponent>
-				</Grid>
-				<Grid item xs={6} sm={3}>
-					<p>Top post</p>
-				</Grid>
+				<Hidden xsDown>
+					<Grid item xs={0} sm={0}  md={4} >
+						<RecomendadosList recomendados={recomendados} />
+					</Grid>
+				</Hidden>
 			</Grid>
 		</div>
-	
 	);
 };
