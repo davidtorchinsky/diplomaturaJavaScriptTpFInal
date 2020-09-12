454a715ed4f30a8a137581398051ebb7439f08a2
Julian <alucard.j.pr@gmail.com>
2020-09-12 17:45:36 -0300

    UserContext y Modal

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import SimpleModal from "./SimpleModal";
 
 import { PostComponent } from "../components/postComponents/PostComponent";
 
 export const HomeScreen = () => {
-	return (
-		<div className='homeContainer'>
-			<Grid container spacing={3}  >
-				<Grid item xs={6} sm={3}>
-					<p>Categorias</p>
-				</Grid>
-				<Grid item xs={12} sm={6} className='postContainer'>
-					<PostComponent></PostComponent>
-				</Grid>
-				<Grid item xs={6} sm={3}>
-					<p>Top post</p>
-				</Grid>
-			</Grid>
-		</div>
-	
-	);
+  return (
+    <div className="homeContainer">
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <p>Categorias</p>
+        </Grid>
+        <Grid item xs={12} sm={6} className="postContainer">
+          <PostComponent></PostComponent>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <p>Top post</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
---
+++ frontend/src/screens/SimpleModal.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { TextField } from "@material-ui/core";
+import { GoogleLogin } from "react-google-login";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 800,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function SimpleModal({ abierto }) {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(abierto);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <div>
+        <img
+          src={require("../assets/images/Capture.PNG")}
+          alt="There should be an image here"
+          align="left"
+        />
+      </div>
+      <div align="center">
+        <h2>Log in</h2>
+        <TextField label="Usuario" className={classes.TextField} />
+        <br />
+        <TextField label="Contraseña" className={classes.TextField} />
+        <br />
+        <br />
+        <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={console.log("oh yea")}
+          onFailure={console.log("oh not yea(?")}
+          cookiePolicy={"single_host_origin"}
+        />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
+
+/**<div>
+      <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div> */
